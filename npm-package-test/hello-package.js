c351b3058adcf5449e4a50c738cfe9b47a6ab16f
miracle9312 <787101215@qq.com>
2018-03-28 17:44:24 +0800

    'test-npm-package'

---
+++ npm-package-test/hello-package.js
@@ -0,0 +1,17 @@
+exports.add = function (val) {
+  return val++;
+};
+
+exports.reduce = function (val) {
+  return val--;
+};
+
+exports.plus = function (val1, val2) {
+  return val1 * val2;
+};
+
+exports.remian = function (val1, val2) {
+  return val1 % val2;
+};
+
+
---
+++ src/test-my-npm.js
@@ -0,0 +1,2 @@
+var test = require('miracle-math-test');
+console.log(test.plus(2,7));
