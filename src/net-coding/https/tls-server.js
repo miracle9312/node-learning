4e8c80194b2afc8610912c574574c8c332b50749
miracle9312 <787101215@qq.com>
2018-04-09 21:45:13 +0800

    'https'

---
+++ src/net-coding/https/tls-server.js
@@ -0,0 +1,21 @@
+var tls = require('tls');
+var fs = require('fs');
+
+var options = {
+  Host:'localhost',
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.crt'),
+  requestCert: true,
+  ca: [fs.readFileSync('./ca.crt')]
+};
+
+var server = tls.createServer(options, function(stream) {
+  console.log('server connected ', stream.authorized?'authorized':'unauthorized');
+  stream.write('welcome!\n');
+  stream.setEncoding('utf8');
+  stream.pipe(stream);
+});
+
+server.listen(8000, function(){
+  console.log('server bound');
+});
