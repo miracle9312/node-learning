d52ee8ec90c0a884cc890574d3fb011fcf18c4b7
miracle9312 <787101215@qq.com>
2018-04-29 17:22:42 +0800

    'cluster'

--- src/net-coding/web-app/process/main.js
+++ src/net-coding/web-app/process/main.js
@@ -1,13 +1,51 @@
 var fork = require('child_process').fork;
 var cpus = require('os').cpus();
 var path = require('path');
-
 var server = require('net').createServer();
+var process = require('process');
+var workers = {};
+var limit = 10;
+var duration = 60000;
+var restart = [];
+
+//限量重启
+var isTooFrequently = function() {
+  var time = new Date.now();
+  var length = restart.push(time);
+  if(length>limit) {
+    restart = restart.slice(limit*-1);
+  }
+
+  return length>=limit && restart[limit-1]-restart[0]<duration;
+};
+
+var createWorker = function() {
+  var worker = fork(path.join(__dirname, '/work.js'));
+  worker.on('message', function(msg) {
+    if(msg.act === 'suicide') {
+      createWorker();
+    }
+  });
+  //自动重启
+  worker.on('exit', function() {
+    console.log('process has exit pid is ' + worker.pid);
+    delete workers[worker.pid];
+  });
+
+  workers[worker.pid] = worker;
+  worker.send('server', server);
+  console.log('create process '+worker.pid);
+};
+
+process.on('exit', function() {
+  for(var pid in workers) {
+    workers[pid].kill();
+  }
+});
+
 server.listen(8000, function() {
-  for(var i = 0, child; i < cpus.length; i++) {
-    child = fork(path.join(__dirname, '/work.js'));
-    child.send('server', server);
+  for(var i = 0; i < cpus.length; i++) {
+    createWorker();
   }
   server.close();
 });
-
--- src/net-coding/web-app/process/work.js
+++ src/net-coding/web-app/process/work.js
@@ -1,14 +1,35 @@
 var http = require('http');
+var process = require('process');
+var logger = require('logger');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200,{'Content-Type': 'text/plain'});
   res.end('http is handled by child, pid is '+process.pid+"\n");
 });
 
+var worker;
 process.on('message', function(m, tcp) {
   if(m=='server') {
-    tcp.on('Connection', function(socket) {
+    worker = tcp;
+    worker.on('connection', function(socket) {
       server.emit('connection', socket);
     })
   }
+});
+
+//捕获异常
+process.on('uncaughtException', function(err) {
+  //记录日志
+  logger.error(err);
+  //自杀信号
+  process.send({act: 'suicide'});
+  //断开连接
+  worker.close(function() {
+    //退出进程
+    process.exit(1);
+  });
+  //长连接超时
+  setTimeout(function(){
+    process.exit(1);
+  },5000)
 });
